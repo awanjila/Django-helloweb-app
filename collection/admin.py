004125a6afd19ccefd1bb8ddaf9a28eec238e30a
awanjila <abemuchikan@gmail.com>
2016-08-22 20:24:59 +0300

    setting up my admin to be able to be accessed by the browsers

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+# import your model
+from collection.models import Thing
+
+#register it
+
+class ThingAdmin(admin.ModelAdmin):
+	model=Thing
+	list_display =('name','description',)
+	prepopulated_fields ={'slug':('name',)
+			}
+admin.site.register(Thing, ThingAdmin)
