857d7e2da8090b1bfcc9fe99161da7ff1744d2ee
awanjila <abemuchikan@gmail.com>
2016-08-22 19:54:26 +0300

    Finished setting up my database which included migrations

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,15 +1,21 @@
 
 from django.conf.urls import patterns,include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
-	url(r'^$', 'collection.views.index', name='home'),
+
+
+
+from django.views.generic import TemplateView
+from collection import views as collection_views
+
+urlpatterns = [
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', collection_views.index, name='home'),
 	url(r'^about/$',
         TemplateView.as_view(template_name='about.html'),
          name='about'),
     url(r'^contact/$', 
         TemplateView.as_view(template_name='contact.html'),
         name='contact'),
-	url(r'^admin/', include(admin.site.urls)),
-	)
+
+	]
