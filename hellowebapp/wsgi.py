f5833c714a53c03cded76c8b0007ea253fe122c6
awanjila <abemuchikan@gmail.com>
2016-08-29 00:58:49 +0300

    Committing before pushing to Heroku.

--- hellowebapp/wsgi.py
+++ hellowebapp/wsgi.py
@@ -12,5 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings")
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
